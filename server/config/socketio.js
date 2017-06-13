c108674fcf877691a047803ef5df1e24d9b8b57f
AndreaDSouza <andrea.dsouza96@gmail.com>
2017-06-13 20:41:00 +0530

    Update

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -27,7 +27,7 @@ function onConnect(socket) {
 
 }
 
-module.exports default function(socketio) {
+export default function(socketio) {
   // socket.io (v1.x.x) is powered by debug.
   // In order to see all the debug output, set DEBUG (in server/config/local.env.js) to including the desired scope.
   //
